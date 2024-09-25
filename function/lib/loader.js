0ada1d15d2cfaab347fa5f222babdeedfbeb4ad2
Ahlul <ahlul963@gmail.com>
2024-09-25 17:07:47 +0700

    refactor code

--- function/lib/loader.js
+++ function/lib/loader.js
@@ -1,27 +1,30 @@
-const { modul } = require('../module');
+const { modul } = require("../module");
 const { fs } = modul;
-const { color } = require('./color')
+const { color } = require("./color");
 
-async function uncache(module = '.') {
-    return new Promise((resolve, reject) => {
-        try {
-            delete require.cache[require.resolve(module)]
-            resolve()
-        } catch (e) {
-            reject(e)
-        }
-    })
+async function uncache(module = ".") {
+  return new Promise((resolve, reject) => {
+    try {
+      delete require.cache[require.resolve(module)];
+      resolve();
+    } catch (e) {
+      reject(e);
+    }
+  });
 }
 
-async function nocache(module, cb = () => { }) {
-    console.log(color('Module', 'blue'), color(`'${module} Di Awasi Lexxy Official'`, 'orange'))
-    fs.watchFile(require.resolve(module), async () => {
-        await uncache(require.resolve(module))
-        cb(module)
-    })
+async function nocache(module, cb = () => {}) {
+  console.log(
+    color("Module", "blue"),
+    color(`'${module} Di Awasi Lexxy Official'`, "orange")
+  );
+  fs.watchFile(require.resolve(module), async () => {
+    await uncache(require.resolve(module));
+    cb(module);
+  });
 }
 
 module.exports = {
-    uncache,
-    nocache
-}
+  uncache,
+  nocache,
+};
