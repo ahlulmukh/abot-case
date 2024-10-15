823d0cc8e92f30a6a7253b8df59c265001e7c7b1
Ahlul <ahlul963@gmail.com>
2024-10-16 03:31:07 +0700

    chore: remove twitter mp3 downloader fiture

--- function/case.js
+++ function/case.js
@@ -1021,36 +1021,6 @@ module.exports = abot = async (abot, m) => {
         }
         break;
 
-      case "twittermp3":
-      case "twtdlmp3":
-        try {
-          if (!text)
-            return m.reply(
-              `Example : ${
-                prefix + command
-              } https://twitter.com/faqeeyaaz/status/1242789155173617664?s=20&t=DRgdl9U8MwTwpY0o1o-96g`
-            );
-          if (text.includes("https://twitter.com/")) {
-            var link = args[0];
-          } else m.reply("Error Link");
-          let { data } = await axios.get(
-            `https://api.aldev.my.id/api/dowloader/twitter?url=${link}&apikey=${global.apikey}`
-          );
-          abot.sendMessage(
-            m.chat,
-            {
-              audio: { url: data.result.audio },
-              mimetype: "audio/mp4",
-            },
-            { quoted: m }
-          );
-        } catch {
-          m.reply(
-            "Maaf Kak Fitur Sedang Error Silahkan Chat Owner Agar Segera Di Perbaiki"
-          );
-        }
-        break;
-
       case "youtubevidio":
       case "ytmp4":
         try {
